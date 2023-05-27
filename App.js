00292b7262d4753517a2f307627537b61bc2ef95
naseeb0 <nasheeb.dangi@gmail.com>
2023-05-27 22:41:35 +0545

    fcom

---
+++ App.js
@@ -0,0 +1,51 @@
+import { StatusBar as ExpoStatusBar } from "expo-status-bar";
+import React from "react";
+import { ThemeProvider } from "styled-components/native";
+import { initializeApp } from "firebase/app";
+
+import {
+  useFonts as useOswald,
+  Oswald_400Regular,
+} from "@expo-google-fonts/oswald";
+import { useFonts as useLato, Lato_400Regular } from "@expo-google-fonts/lato";
+
+import { theme } from "./src/infrastructure/theme";
+import { Navigation } from "./src/infrastructure/navigation";
+
+import { AuthenticationContextProvider } from "./src/services/authentication/authentication.context";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAJwWkiQEfabn6AGf97HZZgpj32uSjPaxw",
+  authDomain: "mealstogo-73c81.firebaseapp.com",
+  projectId: "mealstogo-73c81",
+  storageBucket: "mealstogo-73c81.appspot.com",
+  messagingSenderId: "823797459518",
+  appId: "1:823797459518:web:618de0569146584128bf6f",
+};
+
+initializeApp(firebaseConfig);
+
+export default function App() {
+  const [oswaldLoaded] = useOswald({
+    Oswald_400Regular,
+  });
+
+  const [latoLoaded] = useLato({
+    Lato_400Regular,
+  });
+
+  if (!oswaldLoaded || !latoLoaded) {
+    return null;
+  }
+
+  return (
+    <>
+      <ThemeProvider theme={theme}>
+        <AuthenticationContextProvider>
+          <Navigation />
+        </AuthenticationContextProvider>
+      </ThemeProvider>
+      <ExpoStatusBar style="auto" />
+    </>
+  );
+}
